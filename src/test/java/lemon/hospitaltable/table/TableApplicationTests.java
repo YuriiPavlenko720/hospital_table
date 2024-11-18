ca26cd7a7254937832a6f01767c53ac9c9966270
YuriiPavlenko720 <pavlenko.seo@gmail.com>
2024-11-18 17:42:21 +0200

    18.11.2024 set Docker image of app (try #9)

--- src/test/java/lemon/hospitaltable/table/TableApplicationTests.java
+++ src/test/java/lemon/hospitaltable/table/TableApplicationTests.java
@@ -1,11 +1,9 @@
 package lemon.hospitaltable.table;
 
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Tag("exclude-from-ci")
 class TableApplicationTests {
 
 	@Test
